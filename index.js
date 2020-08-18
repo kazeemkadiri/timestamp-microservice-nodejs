ad2eb51129f2d13c4fd0b5f8f3e9495afd5c261e
Kazeem Kadiri <kazeem.kadiri@gmail.com>
2020-08-18 15:28:49 +0100

    Added code to return invalid date if date string cannot be parsed

--- index.js
+++ index.js
@@ -2,8 +2,20 @@ const express = require("express");
 const app = express();
 
 //Route that returns unix and/or utc timestamp
-app.get("/", function(req, res){
-  res.send("Hello");
+app.get("/api/timestamp/:date_string", function(req, res){
+  const dateString = req.params.date_string;
+  
+  const dateResult = new Date(dateString);
+  
+
+  if(dateResult == "Invalid Date"){
+    res.json({error: "Invalid Date"});
+  }    
+
+  //If date is successfully parsed
+  
+
+
 });
 
 //Configure app to listen on port
