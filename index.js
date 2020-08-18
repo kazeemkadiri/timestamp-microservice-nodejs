121a85788439f4f7fca382dfbf0481e4a56f5269
Kazeem Kadiri <kazeem.kadiri@gmail.com>
2020-08-18 16:13:15 +0100

    Added code to display current time and date if no value is received

--- index.js
+++ index.js
@@ -2,18 +2,23 @@ const express = require("express");
 const app = express();
 
 //Route that returns unix and/or utc timestamp
-app.get("/api/timestamp/:date_string", function(req, res){
+app.get("/api/timestamp/:date_string?", function(req, res){
   const dateString = req.params.date_string;
   
   const dateResult = new Date(dateString);
-  
+//  console.log(dateString);
 
-  if(dateResult == "Invalid Date"){
+  if(dateResult == null){
     res.json({error: "Invalid Date"});
   }    
 
   //If date is successfully parsed
   
+  //If dateString is empty, generate the current date and time
+  if(dateString === undefined){
+    const currentDate = new Date();
+    res.json({unix: currentDate.getTime(), utc: currentDate.toUTCString()});
+  }  
 
 
 });
